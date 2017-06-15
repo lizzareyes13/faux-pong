9ab876ea16477ccc73c49b8702849f1348eb71bb
Lizza Reyes <lizzareyes13@gmail.com>
2017-06-14 23:12:11 -0400

    ballX

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function () {
   console.log("Hello World!");
   canvas = document.getElementById("pongCanvas");
   canvasContext = canvas.getContext("2d");
-  drawEverything();
+
+  setInterval(drawEverything, 50);
 }
 
 function drawEverything () {
+  ballX = ballX + 5;
+
+  console.log(ballX);
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   canvasContext.fillStyle = "white";
   canvasContext.fillRect(225,210,200,200);
   canvasContext.fillStyle = "red";
-  canvasContext.fillRect(canvas.width/2,200,50,25);
+  canvasContext.fillRect(ballX,200,10,10);
 }
