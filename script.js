d4be8118cf2f06729df305089961d7a8a3ee086b
Lizza Reyes <lizzareyes13@gmail.com>
2017-06-14 22:47:40 -0400

    ...

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var canvas;
 var canvasContext;
 
-window.onload = function (){
+window.onload = function () {
   console.log("Hello World!");
   canvas = document.getElementById("pongCanvas");
   canvasContext = canvas.getContext("2d");
+  drawEverything();
+}
+
+function drawEverything () {
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillStyle = "white";
+  canvasContext.fillRect(225,210,200,200);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(canvas.width/2,200,50,25);
 }
