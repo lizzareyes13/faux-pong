284ee0b3028dd07cc14b5e3ea8354575be14cb14
Lizza Reyes <lizzareyes13@gmail.com>
2017-06-22 21:03:40 -0400

    paddle 2

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function drawEverything () {
   colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
  //next line creates the right player paddle
   colorRect(canvas.width - PADDLE_THICKNESS,paddle2Y,
-  PADDLE_THICKNESS,100,'white');
+  PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
   //the line below draws the ball
   colorCirlce(ballX, ballY, 10, 'white');
 }
