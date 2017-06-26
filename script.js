c364e53a7c58eeb7e9753a7b707c52941170de6c
Lizza Reyes <lizzareyes13@gmail.com>
2017-06-26 16:28:50 -0400

    win screen

--- script.js
+++ script.js
@@ -131,9 +131,18 @@ function drawEverything () {
   // the line below fills the screen with black
   colorRect(0,0,canvas.width,canvas.height,'black');
 
+
+
   if(showingWinScreen){
     canvasContext.fillStyle = 'white';
-    canvasContext.fillText("click to continue", 100, 100);
+
+    if(player1Score >= WINNING_SCORE){
+      canvasContext.fillText("Left Player Won!!", 350, 200);
+    }else if(player2Score >= WINNING_SCORE){
+      canvasContext.fillText("Right Player Won!!", 350, 200);
+    }
+
+    canvasContext.fillText("click to continue", 350, 500);
         return;
   }
 
